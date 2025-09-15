58305fad39c603699fd25fa603a4d69305d3d9c3
Fernando Santos <sombarepam@gmail.com>
2025-09-15 16:00:16 -0300

    Feat: cria as queries de select

---
+++ queries/02-selects.sql
@@ -0,0 +1,13 @@
+SELECT * FROM artistas;
+
+SELECT COUNT(*) AS total_artistas FROM artitas;
+
+SELECT nome, "gênero musical" FROM artistas;
+
+SELECT nome, "gênero musical" FROM artistas
+WHERE "País de origem" = 'Brasil';
+
+SELECT nome, "gênero musical" FROM artistas
+WHERE id = 9;
+
+
